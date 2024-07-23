f26f89d943f225a1fe90093e8e02ea894b5739e3
madhu <rayi.madhupriya@gmail.com>
2024-07-24 00:03:51 +0530

    Added Factory Design Pattern

---
+++ src/FactoryPattern/MainClass.java
@@ -0,0 +1,9 @@
+package FactoryPattern;
+
+public class MainClass {
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+        Shape circle = shapeFactory.getShape("Circle");
+        circle.draw();
+    }
+}
